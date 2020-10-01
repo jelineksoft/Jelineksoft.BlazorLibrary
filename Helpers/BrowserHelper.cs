e5c9475f23913ebf1acecad8a7bcd0589d223bc7
Petr Jelínek <developer@jelineksoft.net>
2020-10-01 08:51:02 +0200

    fix input box

--- Helpers/BrowserHelper.cs
+++ Helpers/BrowserHelper.cs
@@ -1,22 +1,41 @@
 ﻿using Microsoft.JSInterop;
 using System.Threading.Tasks;
+using Jelineksoft.Logging;
+using Microsoft.AspNetCore.Components;
 
 namespace Jelineksoft.BlazorLibrary.Helpers
 {
     public class BrowserHelper
     {
         private readonly IJSRuntime xJs;
+        private ILogFactory log;
 
-        public BrowserHelper(IJSRuntime js)
+        public BrowserHelper(IJSRuntime js, ILogFactory logger)
         {
             xJs = js;
+            log = logger;
         }
 
         public async Task<BrowserDimension> GetDimensions()
         {
             return await xJs.InvokeAsync<BrowserDimension>("getDimensions");
         }
-        
+
+        public void FocusElement(ElementReference element)
+        {
+            log?.Trace(this, "BrowserHelper", $"Call FocusElementByRef.");
+            xJs.InvokeVoidAsync("focusElementByRef",element);
+        }
+        public void FocusElement(string id)
+        {
+            log?.Trace(this, "BrowserHelper", $"Call FocusElementById {id}.");
+            xJs.InvokeVoidAsync("focusElementById",id);
+        }
+
+        public async void FocusElementClassOrNextInput(string className)
+        {
+            await xJs.InvokeVoidAsync("focusInputFromBlazor", new[] { className });
+        }
   
     }
 
--- wwwroot/js/browserhelper.js
+++ wwwroot/js/browserhelper.js
@@ -4,3 +4,29 @@
         height: window.innerHeight
     };
 };
+
+window.focusElementByRef = function (element) {
+            element.focus();
+        };
+
+window.focusElementById = function (id) {
+    const element = document.getElementById(id);
+    element.focus();
+}
+
+function focusInputFromBlazor(selector) {
+    var input = document.querySelector(selector);
+    if (!focusInput(input)) {
+        input = input.querySelector("input");
+        focusInput(input);
+    }
+}
+
+function focusInput(input) {
+    if (input && input.focus) {
+        input.focus();
+    }
+    else {
+        return false;
+    }
+}
